e42c9e5750b49282e825ea5419e56ea3464d00fb
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-23 18:11:24 +0100

    change class received/sent

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ createApp({
           name: "Michele",
           avatar: "./img/avatar_1.jpg",
           visible: true,
+          //true = received , false = sent
           messages: [
             {
               date: "10/01/2020 15:30:55",
@@ -34,17 +35,17 @@ createApp({
             {
               date: "20/03/2020 16:30:00",
               message: "Ciao come stai?",
-              status: "sent",
+              status: false,
             },
             {
               date: "20/03/2020 16:30:55",
               message: "Bene grazie! Stasera ci vediamo?",
-              status: "received",
+              status: true,
             },
             {
               date: "20/03/2020 16:35:00",
               message: "Mi piacerebbe ma devo andare a fare la spesa.",
-              status: "sent",
+              status: false,
             },
           ],
         },
@@ -56,17 +57,17 @@ createApp({
             {
               date: "28/03/2020 10:10:40",
               message: "La Marianna va in campagna",
-              status: "received",
+              status: true,
             },
             {
               date: "28/03/2020 10:20:10",
               message: "Sicuro di non aver sbagliato chat?",
-              status: "sent",
+              status: false,
             },
             {
               date: "28/03/2020 16:15:22",
               message: "Ah scusa!",
-              status: "received",
+              status: true,
             },
           ],
         },
@@ -78,12 +79,12 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Lo sai che ha aperto una nuova pizzeria?",
-              status: "sent",
+              status: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Si, ma preferirei andare al cinema",
-              status: "received",
+              status: true,
             },
           ],
         },
@@ -95,12 +96,12 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ricordati di chiamare la nonna",
-              status: "sent",
+              status: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Va bene, stasera la sento",
-              status: "received",
+              status: true,
             },
           ],
         },
@@ -112,17 +113,17 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ciao Claudia, hai novità?",
-              status: "sent",
+              status: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Non ancora",
-              status: "received",
+              status: true,
             },
             {
               date: "10/01/2020 15:51:00",
               message: "Nessuna nuova, buona nuova",
-              status: "sent",
+              status: false,
             },
           ],
         },
@@ -134,12 +135,12 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Fai gli auguri a Martina che è il suo compleanno!",
-              status: "sent",
+              status: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Grazie per avermelo ricordato, le scrivo subito!",
-              status: "received",
+              status: true,
             },
           ],
         },
@@ -151,17 +152,17 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ciao, andiamo a mangiare la pizza stasera?",
-              status: "received",
+              status: true,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
-              status: "sent",
+              status: false,
             },
             {
               date: "10/01/2020 15:51:00",
               message: "OK!!",
-              status: "received",
+              status: true,
             },
           ],
         },
